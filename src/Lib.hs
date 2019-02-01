b0f2134390a93742924e81c2f1c59c4d71f9a31a
Sergey Makarov <setser200018@gmail.com>
2019-02-01 21:53:00 +0300

    Echo bot example from telegram-bot-simple repo

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( start
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Text   (Text)
+import qualified Data.Text as Text
+
+import Telegram.Bot.API
+import Telegram.Bot.Simple
+import Telegram.Bot.Simple.UpdateParser (updateMessageText)
+
+type Model = ()
+
+data Action = NoOp | Echo Text
+
+echoBot :: BotApp Model Action
+echoBot = BotApp 
+    { botInitialModel = ()
+    , botAction = updateToAction
+    , botHandler = handleAction
+    , botJobs = []
+    }
+
+updateToAction :: Update -> Model -> Maybe Action
+updateToAction update _ = 
+    case updateMessageText update of
+        Just text -> Just (Echo text)
+        Nothing -> Nothing
+
+handleAction :: Action -> Model -> Eff Action Model
+handleAction action model = case action of
+    NoOp -> pure model
+    Echo msg -> model <# do
+        replyText msg
+        return NoOp
+
+run :: Token -> IO ()
+run token = do
+    env <- defaultTelegramClientEnv token
+    startBot_ (conversationBot updateChatId echoBot) env
+
+start :: IO ()
+start = do
+    putStrLn "Please, enter Telegram bot's API token:"
+    token <- Token . Text.pack <$> getLine
+    run token
