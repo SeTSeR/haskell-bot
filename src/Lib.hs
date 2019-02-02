f766392a24d33c8e42c5e5b75d365098af7ce442
Sergey Makarov <setser200018@gmail.com>
2019-02-02 19:06:54 +0300

    Add help message

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,37 +3,52 @@ module Lib
     ( start
     ) where
 
+import Control.Applicative
 import           Data.Text   (Text)
 import qualified Data.Text as Text
 
 import Telegram.Bot.API
 import Telegram.Bot.Simple
-import Telegram.Bot.Simple.UpdateParser (updateMessageText)
+import Telegram.Bot.Simple.UpdateParser
 
 type Model = ()
 
-data Action = NoOp | Echo Text
+data Action
+    = NoOp
+    | Echo Text
+    | Show Text
+    deriving (Read, Show)
 
 echoBot :: BotApp Model Action
 echoBot = BotApp 
     { botInitialModel = ()
-    , botAction = updateToAction
+    , botAction = flip updateToAction
     , botHandler = handleAction
     , botJobs = []
     }
 
-updateToAction :: Update -> Model -> Maybe Action
-updateToAction update _ = 
-    case updateMessageText update of
-        Just text -> Just (Echo text)
-        Nothing -> Nothing
+updateToAction :: Model -> Update -> Maybe Action
+updateToAction _ = parseUpdate $
+        Show helpMessage <$  command "start"
+    <|> Show helpMessage <$  command "help"
+    <|> Echo             <$> plainText
+    <|> callbackQueryDataRead
 
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model = case action of
     NoOp -> pure model
+    Show message -> model <# do
+        reply (toReplyMessage message)
+        return NoOp
     Echo msg -> model <# do
         replyText msg
         return NoOp
+
+helpMessage :: Text
+helpMessage = Text.unlines
+    [ "Привет, я @myhlbot, и пока я ничего не умею."
+    , "Можешь попробовать что-нибудь написать."
+    ]
 
 run :: Token -> IO ()
 run token = do
