c059b84fabcd3e4d361b0687458c008513f864bd
Sergey Makarov <setser200018@gmail.com>
2019-02-03 19:18:26 +0300

    Add logging

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,51 +4,90 @@ module Lib
     ) where
 
 import Control.Applicative
+import Control.Monad.Writer
 import           Data.Text   (Text)
 import qualified Data.Text as Text
+
+import Data.ByteString.Lazy(toStrict)
+import qualified Data.Aeson as Aeson
+import Data.Text.Encoding(decodeUtf8)
 
 import Telegram.Bot.API
 import Telegram.Bot.Simple
 import Telegram.Bot.Simple.UpdateParser
 
-type Model = ()
+type Log = Text
+
+type Model = Writer Log ()
 
 data Action
     = NoOp
-    | Echo Text
-    | Show Text
-    deriving (Read, Show)
+    | Show Log Text
+    | QueryLog (Maybe UserId)
+    deriving (Show)
 
 echoBot :: BotApp Model Action
 echoBot = BotApp 
-    { botInitialModel = ()
+    { botInitialModel = return ()
     , botAction = flip updateToAction
     , botHandler = handleAction
     , botJobs = []
     }
 
 updateToAction :: Model -> Update -> Maybe Action
-updateToAction _ = parseUpdate $
-        Show helpMessage <$  command "start"
-    <|> Show helpMessage <$  command "help"
-    <|> Echo             <$> plainText
-    <|> callbackQueryDataRead
+updateToAction _ update =
+    let logMessage = decodeUtf8 . toStrict . Aeson.encode $ update
+        uId = userId <$> (updateMessage update >>= messageFrom)
+        parser = 
+                Show logMessage helpMessage <$  command "start"
+            <|> Show logMessage helpMessage <$  command "help"
+            <|> QueryLog uId                <$  command "log"
+            <|> Show logMessage             <$> plainText
+    in parseUpdate parser update
 
 handleAction :: Action -> Model -> Eff Action Model
-handleAction action model = case action of
-    NoOp -> pure model
-    Show message -> model <# do
-        reply (toReplyMessage message)
-        return NoOp
-    Echo msg -> model <# do
-        replyText msg
-        return NoOp
+handleAction action model =
+    let showmodel message log = model >>
+            (tell log) >>
+            (tell $ Text.pack "\n") >>
+            (tell $ Text.append (Text.pack "Reply:\n") message)
+        logmodel = return ()
+        incorrectusermodel userId = model >>
+            (tell $ Text.unwords
+             [ "User with id"
+             , Text.pack . show $ userId
+             , "has tried to access logs.\n"
+             ])
+    in case action of
+        NoOp -> pure model
+        Show log message -> (showmodel message log) <# do
+            reply (toReplyMessage message)
+            return NoOp
+        QueryLog (Just userId) -> if userId == ownerId
+            then
+                logmodel <# do
+                    let msg = execWriter model
+                    replyText msg
+                    return NoOp
+            else
+                (incorrectusermodel userId) <# do
+                    replyText accessDeniedMessage
+                    return NoOp
+        QueryLog _ -> model <# return NoOp
 
 helpMessage :: Text
 helpMessage = Text.unlines
     [ "Привет, я @myhlbot, и пока я ничего не умею."
     , "Можешь попробовать что-нибудь написать."
     ]
+
+accessDeniedMessage :: Text
+accessDeniedMessage = Text.unlines
+    [ "Доступ к логам разрешён только разработчикам бота."
+    ]
+
+ownerId :: UserId
+ownerId = UserId 205887307
 
 run :: Token -> IO ()
 run token = do
