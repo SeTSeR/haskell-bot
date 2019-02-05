ae04b8c6252f28677f04c681e4ec36e29fa06269
Sergey Makarov <setser200018@gmail.com>
2019-02-05 18:04:27 +0300

    Add storing users info

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,23 +14,32 @@ import Data.ByteString.Lazy(toStrict)
 import qualified Data.Aeson as Aeson
 import Data.Text.Encoding(decodeUtf8)
 
+import Data.Hashable
+import           Data.HashMap.Strict   (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+
 import Telegram.Bot.API
 import Telegram.Bot.Simple
 import Telegram.Bot.Simple.UpdateParser
 
 type Log = Text
+type Username = Text
 
-type Model = Writer Log ()
+type Model = Writer Log (HashMap UserId Chat)
+
+instance Hashable UserId where
+    hashWithSalt num (UserId uid) = hashWithSalt num uid
 
 data Action
     = NoOp
+    | Start (Maybe UserId) (Maybe Chat)
     | Show Log Text
     | QueryLog (Maybe UserId)
     deriving (Show)
 
 echoBot :: BotApp Model Action
 echoBot = BotApp 
-    { botInitialModel = return ()
+    { botInitialModel = return HashMap.empty
     , botAction = flip updateToAction
     , botHandler = handleAction
     , botJobs = []
@@ -40,8 +49,9 @@ updateToAction :: Model -> Update -> Maybe Action
 updateToAction _ update =
     let logMessage = decodeUtf8 . toStrict . Aeson.encode $ update
         uId = userId <$> (updateMessage update >>= messageFrom)
+        chat = messageChat <$> updateMessage update
         parser = 
-                Show logMessage helpMessage <$  command "start"
+                Start uId chat              <$  command "start"
             <|> Show logMessage helpMessage <$  command "help"
             <|> QueryLog uId                <$  command "log"
             <|> Show logMessage pongMessage <$  command "ping"
@@ -50,16 +60,23 @@ updateToAction _ update =
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model =
     let showmodel message log = do
-            _ <- model
+            users <- model
             tell log
             tell $ Text.pack "\n"
             tell $ Text.append (Text.pack "Reply:\n") message
+            return users
         logmodel = botInitialModel echoBot
         incorrectusermodel userId = do
-            _ <- model
+            users <- model
             tell $ accessMessage userId
+            return users
+        updatedmodel (Just uid) (Just chat) = HashMap.insert uid chat <$> model
+        updatedmodel _ _ = model
     in case action of
         NoOp -> pure model
+        Start uid chat -> (updatedmodel uid chat) <# do
+            replyText helpMessage
+            return NoOp
         Show log message -> (showmodel message log) <# do
             reply (toReplyMessage message)
             return NoOp
